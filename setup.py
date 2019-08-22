715423e41d3fa9655e1e42335911e7c808de3fb3
yxxhero <yxxhero@example.com>
2019-08-22 21:21:18 +0800

    bugfix

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 filepath = "uitable/README.md"
 
 setup(
     name="uitable",
-    version="0.2",
+    version="0.3",
     description="display data by table format",
     long_description=open(filepath).read(),
     long_description_content_type="text/markdown",
@@ -12,6 +12,12 @@ setup(
     author="yxxhero",
     author_email="aiopsclub@163.com",
     license="MIT",
-    packages=["uitable"],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
     zip_safe=False,
 )
--- uitable/README.md
+++ uitable/README.md
@@ -6,4 +6,8 @@ uitable_ins = UiTable()
 uitable_ins.add_header("name", "age")
 uitable_ins.add_row("yxx", "12")
 print(uitable_ins)
+
+# output:
+name   age    
+yxx    12  
 ```
